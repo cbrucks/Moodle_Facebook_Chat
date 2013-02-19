c3e83a8fc82e770431424fc3a8963195e31b3897
Chris Brucks <chris.brucks@rackspace.com>
2013-02-19 23:36:46 +0000

    chat up but url doesnt work

--- block_fb_chat.php
+++ block_fb_chat.php
@@ -28,16 +28,30 @@ class block_fb_chat extends block_base {
         $this->content->text  = '<div id="fb-root"></div>';
         $this->content->text .= '<script>(function(d, s, id) {';
         $this->content->text .= 'var js, fjs = d.getElementsByTagName(s)[0];';
-        $this->content->text .= 'if (d.getElementById(id)) return;';  // course
+        $this->content->text .= 'if (d.getElementById(id)) return;';
         $this->content->text .= 'js = d.createElement(s); js.id = id;';
         $this->content->text .= 'js.src = "//connect.facebook.net/en_US/all.js#xfbml=1";';
         $this->content->text .= 'fjs.parentNode.insertBefore(js, fjs);';
-        $this->content->text .= '}(document, 'script', 'facebook-jssdk'));</script>';
-        $this->content->text .= '<div class="fb-comments" data-href="http://example.com" data-width="470" data-num-posts="10"></div>';
+        $this->content->text .= '}(document, "script", "facebook-jssdk"));</script>';
+        $this->content->text .= '<div class="fb-comments" data-href="' . $this->curPageURL() . '" data-width="175" data-num-posts="10"></div>';
+
+        echo $OUTPUT->notification($this->curPageURL());
 
         return $this->content;
     }
-
+    function curPageURL() {
+        $pageURL = 'http';
+        if ($_SERVER["HTTPS"] == "on") {
+            $pageURL .= "s";
+        }
+        $pageURL .= "://";
+        if ($_SERVER["SERVER_PORT"] != "80") {
+            $pageURL .= $_SERVER["SERVER_NAME"].":".$_SERVER["SERVER_PORT"].$_SERVER["REQUEST_URI"];
+        } else {
+            $pageURL .= $_SERVER["SERVER_NAME"].$_SERVER["REQUEST_URI"];
+        }
+        return $pageURL;
+    }
     function applicable_formats() {
         return array('site' => true, 'course' => true);
     }
